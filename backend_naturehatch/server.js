c79e607e2071eff0887c00e2cb6af26db0eadeae
VVinaykumar003 <119816918+VVinaykumar003@users.noreply.github.com>
2025-07-15 15:59:05 +0530

    cors

--- backend_naturehatch/server.js
+++ backend_naturehatch/server.js
@@ -53,34 +53,20 @@ mongoose
   app.use(fileUpload({
     useTempFiles: true,
   }));
-  // app.use(cors({
-  //   origin: isProduction ? process.env.CLIENT_URL : "http://localhost:5174",
-  //   credentials: true,
-  // }));
+
 
   const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
 
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true
-// }));
+
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:5174',process.env.FRONTEND_URL],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-// app.get("/", (req, res) => {
-//   res.send("Hello, World!");
-// });
+
 
 app.use("/api/admin", require("./routes/adminRoutes"));
 app.use("/api/user",require("./routes/userRoutes"));
