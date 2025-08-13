3457d33639f17c0da02c24b47b487d82bee5ed4b
Sujal Raj <sujalraj2064@gmail.com>
2025-08-13 13:14:16 +0530

    Implement order status update functionality and enhance Order component to reflect status changes

--- backend_naturehatch/routes/orderRoutes.js
+++ backend_naturehatch/routes/orderRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const {createOrder,getAllOrders ,placeOrderByPaypal,allOrders} = require("../controllers/orderController")
+const {createOrder,getAllOrders ,placeOrderByPaypal,allOrders, updateOrderStatus} = require("../controllers/orderController")
 const authMiddleware = require("../middlewares/userAuthMiddleware")
 const adminAuthMiddleware = require("../middlewares/adminAuthMiddleware");
 
 router.post('/create-order', authMiddleware, createOrder);
 router.get('/get-all-orders', getAllOrders);
+router.put('/update-status/:orderId', updateOrderStatus);
 // router.get('/get-order/:id', authMiddleware, getOrderById);
 
 // Place order by PayPal
